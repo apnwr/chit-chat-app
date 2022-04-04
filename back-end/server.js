f82ef7149d57985af664f465d8db905e5a759023
ayush50 <ayush@grappus.com>
2022-04-04 14:03:15 +0530

    [ADD] production config added for deployment

--- back-end/server.js
+++ back-end/server.js
@@ -7,7 +7,8 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes"); 
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const {notFound, errorHandler} = require("./middleware/errorMiddleWare")
+const { notFound, errorHandler } = require("./middleware/errorMiddleWare")
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -21,13 +22,29 @@ app.use(express.json());
 
 
 
-app.get('/', (req, res) => {
-    res.send("API is running successfully !!");
-})
+
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------Deployment----------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/front-end/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "front-end", "build", "index.html"));
+    })
+} else {
+    app.get("/", (req, res) => {
+      res.send("API is running successfully !!");
+    });
+}
+
+
+// --------------------Deployment----------------
 
 //error middlewares
 app.use(notFound);
